53647897ec00f51ee9764a54dde111f8d0b180e6
Rene5891 <97186151+Rene5891@users.noreply.github.com>
2022-05-31 13:26:10 +0500

    Update Open_Read_file_on_python.py

--- Open_Read_file_on_python.py
+++ Open_Read_file_on_python.py
@@ -1,8 +1,8 @@
 file_name = input('Введите имя файла - ')
 file_1 = open(file_name, 'r', encoding='utf-8')
 #line = file_1.readline()
-#while line != '':              # пока не конец файла
-#    print(line.strip())        # обрабатываем считанную строку
+#while line != '':              
+#    print(line.strip())        
 #    line = file_1.readline()
 #for line in file_1:
 #    print(line.strip())
